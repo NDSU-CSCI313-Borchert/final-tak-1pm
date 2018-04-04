ca8deca536b19e7c73d152eca94dcda85d9efdc4
Skyler Svendsen <skyler.svendsen@ndsu.edu>
2018-04-04 15:26:50 -0500

    created tests directory and a board test script

---
+++ Tests/test_Board.py
@@ -0,0 +1,10 @@
+import unittest
+from board import *
+
+class test_Board(unittest.TestCase):
+    def test_Board_can_be_created(self):
+        board = Board()
+        self.assertTrue(True)
+
+    if __name__ == '__main__':
+        unittest.main()
