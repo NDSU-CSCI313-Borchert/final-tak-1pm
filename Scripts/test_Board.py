2df7414d0e4014972c04062b8ae08acd770f3c84
Skyler Svendsen <skylersvendsen@gmail.com>
2018-04-06 15:31:10 -0500

    reverting back to unittest

--- Scripts/test_Board.py
+++ Scripts/test_Board.py
@@ -1,8 +1,7 @@
-
+import unittest
 from board import *
 
-def test_board_can_be_created():
-    board = Board("5x5")
-    board.rect.x =120
-    board.rect.y =120
-    assert test_board_can_be_created(True) == True
+class test_Board(unittest.TestCase):
+    def test_board_can_be_created(self):
+        board = Board("5x5")
+        self.assertTrue(True)
