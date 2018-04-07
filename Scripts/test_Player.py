8d20bb2a3ebcc71191de66072f84f53b0c40490c
Skyler Svendsen <skylersvendsen@gmail.com>
2018-04-06 20:38:06 -0500

    Display pieces and remaining pieces count

--- Scripts/test_Player.py
+++ Scripts/test_Player.py
@@ -13,3 +13,13 @@ class test_Player(unittest.TestCase):
     def test_player_has_correct_capstones(self):
         player = Player()
         self.assertEqual(player.capstones, 1)
+
+    def test_player_has_correct_stones_after_removing_stone(self):
+        player = Player()
+        player.removeStone()
+        self.assertEqual(player.stones, 20)
+
+    def test_player_has_correct_capstones_after_removing_capstone(self):
+        player = Player()
+        player.removeCapstone()
+        self.assertEqual(player.capstones, 0)
