9a6215ffdce86f38724b8e99c26481987e21a426
Skyler Svendsen <skylersvendsen@gmail.com>
2018-04-25 21:42:09 -0500

    fixing player tests

--- Scripts/test_Player.py
+++ Scripts/test_Player.py
@@ -3,23 +3,23 @@ from player import *
 
 class test_Player(unittest.TestCase):
     def test_player_can_be_created(self):
-        player = Player()
+        player = Player(21, 1)
         self.assertTrue(True)
 
     def test_player_has_correct_stones(self):
-        player = Player()
+        player = Player(21, 1)
         self.assertEqual(player.stones, 21)
 
     def test_player_has_correct_capstones(self):
-        player = Player()
+        player = Player(21, 1)
         self.assertEqual(player.capstones, 1)
 
     def test_player_has_correct_stones_after_removing_stone(self):
-        player = Player()
+        player = Player(21, 1)
         player.removeStone()
         self.assertEqual(player.stones, 20)
 
     def test_player_has_correct_capstones_after_removing_capstone(self):
-        player = Player()
+        player = Player(21, 1)
         player.removeCapstone()
         self.assertEqual(player.capstones, 0)
