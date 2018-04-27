f8ea5fe9563ad7a1cecc792a3da7c317d457a876
skylersvendsen <35610154+skylersvendsen@users.noreply.github.com>
2018-04-27 13:35:01 -0500

    Added tests to test all board types and sizes

--- Scripts/test_Board.py
+++ Scripts/test_Board.py
@@ -3,9 +3,86 @@ import unittest
 from board import *
 
 class test_Board(unittest.TestCase):
-    def test_board_can_be_created(self):
+    def test_5x5_brown_board_can_be_created(self):
         size = "5x5"
         design = "Brown"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_4x4_brown_board_can_be_created(self):
+        size = "4x4"
+        design = "Brown"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_3x3_brown_board_can_be_created(self):
+        size = "3x3"
+        design = "Brown"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+     def test_5x5_space_board_can_be_created(self):
+        size = "5x5"
+        design = "Space"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_4x4_space_board_can_be_created(self):
+        size = "4x4"
+        design = "Space"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_3x3_space_board_can_be_created(self):
+        size = "3x3"
+        design = "Space"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_5x5_yellow_board_can_be_created(self):
+        size = "5x5"
+        design = "Yellow"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_4x4_yellow_board_can_be_created(self):
+        size = "4x4"
+        design = "Yellow"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_3x3_yellow_board_can_be_created(self):
+        size = "3x3"
+        design = "Yellow"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_5x5_summer_board_can_be_created(self):
+        size = "5x5"
+        design = "Summerbreeze"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_4x4_summer_board_can_be_created(self):
+        size = "4x4"
+        design = "Summerbreeze"
+        board_type = str(size) + str(design)
+        board = Board(board_type)
+        self.assertTrue(True)
+        
+    def test_3x3_summer_board_can_be_created(self):
+        size = "3x3"
+        design = "Summerbreeze"
         board_type = str(size) + str(design)
         board = Board(board_type)
         self.assertTrue(True)
