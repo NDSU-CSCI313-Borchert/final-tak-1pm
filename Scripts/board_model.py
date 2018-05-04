d66d75e9670ddba35091cf8e93fd58ad9966182d
Michael Footitt <michaei.footitt@ndsu.edu>
2018-05-04 13:49:53 -0500

    have verything here just have an indent error if you guys could take a look

--- Scripts/board_model.py
+++ Scripts/board_model.py
@@ -570,6 +570,7 @@ class BoardModel():
             return False
 
     # pos x from the mouse, position y from the mouse, a boolean stating weather or not it is player1, then if the stone is a standing stone.
+'''
     def Mark_spot(self, posx, posy, b,standingStone):
         
         """
@@ -676,147 +677,8 @@ class BoardModel():
             self.markGrid[4][4] = mark
     #print.print(self.markGrid)
     #Trying something new I suppose, whenever a capstone is placed there is a second matrix where the info is stored to help fro when being picked up
-    def mark_grid2(self,posx,posy,piece, color):
-        b = False
-        position = self.get_square(posx, posy)
-        if piece =="flat" and color =="brown":
-            mark ='X'
-        elif piece =="flat" and color =="beige":
-            mark ='Y'
-        elif piece =="wall":
-            mark ='Z'
-        elif piece =="capstone" and color =="brown":
-            mark='X'
-            b=True
-        if position == "00":
-            self.stack_0_0_model.append(self.markGrid[0][0])
-            self.markGrid[0][0] = mark
-            if b ==True:
-                self.capstoneSpots[0][0] = mark
-        elif position == "10":
-            self.stack_1_0_model.append(self.markGrid[1][0])
-            self.markGrid[1][0] = mark
-            if b ==True:
-                self.capstoneSpots[1][0] = mark
-        elif position == "20":
-            self.stack_2_0_model.append(self.markGrid[2][0])
-            self.markGrid[2][0] = mark
-            if b ==True:
-                self.capstoneSpots[2][0] = mark
-        elif position == "30":
-            self.stack_3_0_model.append(self.markGrid[3][0])
-            self.markGrid[3][0] = mark
-            if b ==True:
-                self.capstoneSpots[3][0] = mark
-        elif position == "40":
-            self.stack_4_0_model.append(self.markGrid[4][0])
-            self.markGrid[4][0] = mark
-            if b ==True:
-                self.capstoneSpots[4][0] = mark
-
-        elif position == "01":
-            self.stack_0_1_model.append(self.markGrid[0][1])
-            self.markGrid[0][1] = mark
-            if b ==True:
-                self.capstoneSpots[0][1] = mark
-        elif position == "11":
-            self.stack_1_1_model.append(self.markGrid[1][1])
-            self.markGrid[1][1] = mark
-            if b ==True:
-                self.capstoneSpots[1][1] = mark
-        elif position == "21":
-            self.stack_2_1_model.append(self.markGrid[2][1])
-            self.markGrid[2][1] = mark
-            if b ==True:
-                self.capstoneSpots[2][1] = mark
-        elif position == "31":
-            self.stack_3_1_model.append(self.markGrid[3][1])
-            self.markGrid[3][1] = mark
-            if b ==True:
-                self.capstoneSpots[3][1] = mark
-        elif position == "41":
-            self.stack_4_1_model.append(self.markGrid[4][1])
-            self.markGrid[4][1] = mark
-            if b ==True:
-                self.capstoneSpots[4][1] = mark
-
-        elif position == "02":
-            self.stack_0_2_model.append(self.markGrid[0][2])
-            self.markGrid[0][2] = mark
-            if b ==True:
-                self.capstoneSpots[0][2] = mark
-        elif position == "12":
-            self.stack_1_2_model.append(self.markGrid[1][2])
-            self.markGrid[1][2] = mark
-            if b ==True:
-                self.capstoneSpots[1][2] = mark
-        elif position == "22":
-            self.stack_2_2_model.append(self.markGrid[2][2])
-            self.markGrid[2][2] = mark
-            if b ==True:
-                self.capstoneSpots[2][2] = mark
-        elif position == "32":
-            self.stack_3_2_model.append(self.markGrid[3][2])
-            self.markGrid[3][2] = mark
-            if b ==True:
-                self.capstoneSpots[3][2] = mark
-        elif position == "42":
-            self.stack_4_2_model.append(self.markGrid[4][2])
-            self.markGrid[4][2] = mark
-            if b ==True:
-                self.capstoneSpots[4][2] = mark
-
-        elif position == "03":
-            self.stack_0_3_model.append(self.markGrid[0][3])
-            self.markGrid[0][3] = mark
-            if b ==True:
-                self.capstoneSpots[0][3] = mark
-        elif position == "13":
-            self.stack_1_3_model.append(self.markGrid[1][3])
-            self.markGrid[1][3] = mark
-            if b ==True:
-                self.capstoneSpots[1][3] = mark
-        elif position == "23":
-            self.stack_2_3_model.append(self.markGrid[2][3])
-            self.markGrid[2][3] = mark
-            if b ==True:
-                self.capstoneSpots[2][3] = mark
-        elif position == "33":
-            self.stack_3_3_model.append(self.markGrid[3][3])
-            self.markGrid[3][3] = mark
-            if b ==True:
-                self.capstoneSpots[3][3] = mark
-        elif position == "43":
-            self.stack_4_3_model.append(self.markGrid[4][3])
-            self.markGrid[4][3] = mark
-            if b ==True:
-                self.capstoneSpots[4][3] = mark
-
-        elif position == "04":
-            self.stack_0_4_model.append(self.markGrid[0][4])
-            self.markGrid[0][4] = mark
-            if b ==True:
-                self.capstoneSpots[0][4] = mark
-        elif position == "14":
-            self.stack_1_4_model.append(self.markGrid[1][4])
-            self.markGrid[1][4] = mark
-            if b ==True:
-                self.capstoneSpots[1][4] = mark
-        elif position == "24":
-            self.stack_2_4_model.append(self.markGrid[2][4])
-            self.markGrid[2][4] = mark
-            if b ==True:
-                self.capstoneSpots[2][4] = mark
-        elif position == "34":
-            self.stack_3_4_model.append(self.markGrid[3][4])
-            self.markGrid[3][4] = mark
-            if b ==True:
-                self.capstoneSpots[3][4] = mark
-        elif position == "44":
-            self.stack_4_4_model.append(self.markGrid[4][4])
-            self.markGrid[4][4] = mark
-            if b ==True:
-                self.capstoneSpots[4][4] = mark
+    
+'''
 
     def marking(self,position,name,color):
         if name == "flat" and color == "brown":
@@ -937,7 +799,7 @@ class BoardModel():
             self.markGrid[4][4] = mark
 
 
-    def mark_spot3(self,stack00,stack10,stack20,stack30,stack40,stack01,stack11,stack21,stack31,stack41,stack02,stack12,stack22,stack32,stack42,stack03,stack13,stack23,stack33,stack43,stack04,stack14,stack24,stack34,stack44):
+    def mark_grid(self,stack00,stack10,stack20,stack30,stack40,stack01,stack11,stack21,stack31,stack41,stack02,stack12,stack22,stack32,stack42,stack03,stack13,stack23,stack33,stack43,stack04,stack14,stack24,stack34,stack44):
         #Not done tried to do in the morning, will finish in class during first 5 minutes
         self.marking(stack00[len(stack00)-3],stack00[len(stack00)-2],stack00[len(stack00)-1])
         self.marking(stack10[len(stack10) - 3], stack10[len(stack10) - 2], stack10[len(stack10) - 1])
@@ -948,6 +810,22 @@ class BoardModel():
         self.marking(stack11[len(stack11) - 3], stack11[len(stack11) - 2], stack11[len(stack11) - 1])
         self.marking(stack21[len(stack21) - 3], stack21[len(stack21) - 2], stack21[len(stack21) - 1])
         self.marking(stack31[len(stack31) - 3], stack31[len(stack31) - 2], stack31[len(stack31) - 1])
+        self.marking(stack41[len(stack41) - 3], stack41[len(stack41) - 2], stack41[len(stack41) - 1])
+        self.marking(stack02[len(stack02) - 3], stack02[len(stack02) - 2], stack02[len(stack02) - 1])
+        self.marking(stack12[len(stack12) - 3], stack12[len(stack12) - 2], stack12[len(stack12) - 1])
+        self.marking(stack22[len(stack22) - 3], stack22[len(stack22) - 2], stack22[len(stack22) - 1])
+        self.marking(stack32[len(stack32) - 3], stack32[len(stack32) - 2], stack32[len(stack32) - 1])
+        self.marking(stack42[len(stack42) - 3], stack42[len(stack42) - 2], stack42[len(stack42) - 1])
+        self.marking(stack03[len(stack03) - 3], stack03[len(stack03) - 2], stack03[len(stack03) - 1])
+        self.marking(stack13[len(stack13) - 3], stack13[len(stack13) - 2], stack13[len(stack13) - 1])
+        self.marking(stack23[len(stack23) - 3], stack23[len(stack23) - 2], stack23[len(stack23) - 1])
+        self.marking(stack33[len(stack33) - 3], stack33[len(stack33) - 2], stack33[len(stack33) - 1])
+        self.marking(stack43[len(stack43) - 3], stack43[len(stack43) - 2], stack43[len(stack43) - 1])
+        self.marking(stack04[len(stack04) - 3], stack04[len(stack04) - 2], stack04[len(stack04) - 1])
+        self.marking(stack14[len(stack14) - 3], stack14[len(stack14) - 2], stack14[len(stack14) - 1])
+        self.marking(stack24[len(stack24) - 3], stack24[len(stack24) - 2], stack24[len(stack24) - 1])
+        self.marking(stack34[len(stack34) - 3], stack34[len(stack34) - 2], stack34[len(stack34) - 1])
+        self.marking(stack44[len(stack44) - 3], stack44[len(stack44) - 2], stack44[len(stack44) - 1])
 
 
 
